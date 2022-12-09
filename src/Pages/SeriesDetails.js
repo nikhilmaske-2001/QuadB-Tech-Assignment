ded4731985c1184e0439ec0a7e3b7b377bd4a147
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2022-12-09 17:28:52 +0530

    Added some paramters in details.

--- src/Pages/SeriesDetails.js
+++ src/Pages/SeriesDetails.js
@@ -8,6 +8,7 @@ const SeriesDetails = () => {
     const [image, setImage] = useState();
     const [score, setScore] = useState(0);
     const [data, setData] = useState({});
+    const [language, setLanguage] = useState("");
 
     const updateData = (e) => {
         setData({
@@ -27,7 +28,8 @@ const SeriesDetails = () => {
                 }
             });
             setName(series.show.name);
-            setSummary(series.show.summary);
+            setSummary(series.show.summary.replace(/<[^>]+>/g, ''));
+            setLanguage(series.show.language);
             setImage(series.show.image.original);
             setScore(series.show.rating.average);
         }
@@ -44,12 +46,12 @@ const SeriesDetails = () => {
 
     return (
         <section className="text-gray-700 body-font overflow-hidden bg-white">
-            <div className="container px-5 py-24 mx-auto">
+            <div className="container px-5 py-2 mx-auto">
                 <div className="lg:w-4/5 mx-auto flex flex-wrap">
                     <img alt="ecommerce" className="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200" src={image} />
                     <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-                        <h2 className="text-sm title-font text-gray-500 tracking-widest">{name}</h2>
-                        <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">The Catcher in the Rye</h1>
+                        <h2 className="text-sm title-font text-gray-500 tracking-widest">{language}</h2>
+                        <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{name}</h1>
                         <div className="flex mb-4">
                             <span className="flex items-center">
                                 <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-red-500" viewBox="0 0 24 24">
@@ -89,28 +91,6 @@ const SeriesDetails = () => {
                         </div>
                         <p className="leading-relaxed">{summary}</p>
                         <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
-                            <div className="flex">
-                                <span className="mr-3">Color</span>
-                                <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
-                                <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
-                                <button className="border-2 border-gray-300 ml-1 bg-red-500 rounded-full w-6 h-6 focus:outline-none"></button>
-                            </div>
-                            <div className="flex ml-6 items-center">
-                                <span className="mr-3">Size</span>
-                                <div className="relative">
-                                    <select className="rounded border appearance-none border-gray-400 py-2 focus:outline-none focus:border-red-500 text-base pl-3 pr-10">
-                                        <option>SM</option>
-                                        <option>M</option>
-                                        <option>L</option>
-                                        <option>XL</option>
-                                    </select>
-                                    <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-                                        <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4" viewBox="0 0 24 24">
-                                            <path d="M6 9l6 6 6-6"></path>
-                                        </svg>
-                                    </span>
-                                </div>
-                            </div>
                         </div>
                         <div className="flex">
                             <span className="title-font font-medium text-2xl text-gray-900">$58.00</span>
