163000ab51108aff2753b0142d9ab6357f60666b
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2022-12-09 19:50:06 +0530

    Fix some ui.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -49,8 +49,7 @@ const Model = ({ name, language }) => {
                                     <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
                                         First Name
                                     </label>
-                                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" name="name" onChange={updateData} />
-                                    <p className="text-red-500 text-xs italic">Please fill out this field.</p>
+                                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="text" placeholder="Jane" name="name" onChange={updateData} />
                                 </div>
                                 <div className="w-full md:w-1/2 px-3">
                                     <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
@@ -62,15 +61,15 @@ const Model = ({ name, language }) => {
                             <div className="flex flex-wrap -mx-3 mb-2">
                                 <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
                                     <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
-                                        City
+                                        Age
                                     </label>
-                                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="Albuquerque" name="city" onChange={updateData} />
+                                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="18" name="city" onChange={updateData} />
                                 </div>
                                 <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
                                     <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
-                                        Zip
+                                        City
                                     </label>
-                                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="90210" name="zip" onChange={updateData} />
+                                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="Ludhiana" name="zip" onChange={updateData} />
                                 </div>
                             </div>
                         </form>
